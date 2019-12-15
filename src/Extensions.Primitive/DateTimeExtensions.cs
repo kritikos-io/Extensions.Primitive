46715fedc9d9528727d09e5c03322f8e94fe7b8d
Alexandros Kritikos <akritikos@outlook.com>
2019-12-15 22:44:21 +0200

    :triangular_flag_on_post: Removes Unix epoch conversions, provided natively for DateTimeOffset

--- src/Extensions.Primitive/DateTimeExtensions.cs
+++ src/Extensions.Primitive/DateTimeExtensions.cs
@@ -4,41 +4,5 @@ namespace Kritikos.Extensions.Primitive
 
 	public static class DateTimeExtensions
 	{
-		/// <summary>
-		/// Ticks representation of the Unix Epoch time: 00:00:00 UTC Thursday, 1 January 1970.
-		/// </summary>
-		private const long EpochTicks = 621355968000000000;
-
-		/// <summary>
-		/// Converts a <see cref="DateTime"/> object to UNIX epoch format.
-		/// </summary>
-		/// <param name="date">The <see cref="DateTime"/> to convert.</param>
-		/// <returns>The number of seconds that have ellapsed since Unix epoch.</returns>
-		public static long ToEpochTime(this DateTime date)
-			=> Convert.ToInt64((date.ToUniversalTime().Ticks - EpochTicks) / TimeSpan.TicksPerSecond);
-
-		/// <summary>
-		/// Converts a <see cref="DateTimeOffset"/> object to UNIX epoch format.
-		/// </summary>
-		/// <param name="date">The <see cref="DateTimeOffset"/> to convert.</param>
-		/// <returns>The number of seconds that have ellapsed since Unix epoch.</returns>
-		public static long ToEpochTime(this DateTimeOffset date)
-			=> Convert.ToInt64((date.ToUniversalTime().Ticks - EpochTicks) / TimeSpan.TicksPerSecond);
-
-		/// <summary>
-		/// Converts epoch time to a proper <see cref="DateTime"/> object.
-		/// </summary>
-		/// <param name="secondsSinceEpoch">The number of seconds that have ellapsed since Unix epoch.</param>
-		/// <returns><see cref="DateTime"/> representation of <paramref name="secondsSinceEpoch"/>.</returns>
-		public static DateTime FromEpochTime(this long secondsSinceEpoch)
-			=> new DateTime(EpochTicks + (secondsSinceEpoch * TimeSpan.TicksPerSecond));
-
-		/// <summary>
-		/// Converts epoch time to a proper <see cref="DateTimeOffset"/> object.
-		/// </summary>
-		/// <param name="secondsSinceEpoch">The number of seconds that have ellapsed since Unix epoch.</param>
-		/// <returns><see cref="DateTimeOffset"/> representation of <paramref name="secondsSinceEpoch"/>.</returns>
-		public static DateTimeOffset FromEpochTimeOffset(this long secondsSinceEpoch)
-			=> new DateTime(EpochTicks + (secondsSinceEpoch * TimeSpan.TicksPerSecond), DateTimeKind.Utc);
 	}
 }
