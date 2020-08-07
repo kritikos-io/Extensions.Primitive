9a424ea80bad9cc0f5992a5f472e4278bd365387
Alexandros Kritikos <akritikos@outlook.com>
2020-08-07 18:59:27 +0300

    :sparkles: Adds simple DateTime/DateTimeOffset extensions

--- src/Extensions.Primitive/DateTimeExtensions.cs
+++ src/Extensions.Primitive/DateTimeExtensions.cs
@@ -1,8 +1,39 @@
 namespace Kritikos.Extensions.Primitive
 {
 	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics.Contracts;
+	using System.Linq;
 
 	public static class DateTimeExtensions
 	{
+		public static readonly DateTimeOffset GregorianCalendarStart
+			= new DateTimeOffset(1582, 10, 15, 0, 0, 0, TimeSpan.Zero);
+
+		[Pure]
+		public static bool IsWeekend(this DateTime date)
+			=> date.DayOfWeek == DayOfWeek.Saturday
+				|| date.DayOfWeek == DayOfWeek.Sunday;
+
+		[Pure]
+		public static bool IsWeekend(this DateTimeOffset date)
+			=> date.DayOfWeek == DayOfWeek.Saturday
+				|| date.DayOfWeek == DayOfWeek.Sunday;
+
+		[Pure]
+		public static bool IsLeapYear(this DateTime date)
+			=> DateTime.DaysInMonth(date.Year, 2) == 29;
+
+		[Pure]
+		public static bool IsLeapYear(this DateTimeOffset date)
+			=> DateTime.DaysInMonth(date.Year, 2) == 29;
+
+		[Pure]
+		public static bool Between(this DateTime dateTime, DateTime from, DateTime to)
+			=> dateTime >= from && dateTime <= to;
+
+		[Pure]
+		public static bool Between(this DateTimeOffset dateTime, DateTimeOffset from, DateTimeOffset to)
+			=> dateTime >= from && dateTime <= to;
 	}
 }
